6a9321bddde6888ae37df99efcfca6f9fcce2b18
lokidv <120049519+lokidv@users.noreply.github.com>
2023-10-18 10:58:29 +0300

    Update main.js
    add restart service

--- main.js
+++ main.js
@@ -67,7 +67,10 @@ async function startHttpServer() {
 async function addVpn(req, res, query){
 
 
+        let restart = await shell.exec('sudo systemctl restart bvpn.service',{async:true})
+        logger.info('restart status ',restart);
 
+        await sleep(1000)
 
 
   let file_is_exist = await  fs.existsSync("/root/"+query.publicKey+".ovpn")
@@ -145,6 +148,14 @@ if (_result.code === 0) {
 
 
 async function removeVpn(req, res, query){
+
+
+
+ let restart = await shell.exec('sudo systemctl restart bvpn.service',{async:true})
+        logger.info('restart status ',restart);         
+
+        await sleep(1000)
+
 
  const result = shell.exec('/home/bvpn/openvpn-install.sh', { async: true });
 
